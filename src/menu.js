d34a4054b254b53ea8a3429dd154ebaf4d049057
Aleksa Petrovic <aleksapetrovic2907@gmail.com>
2025-01-08 19:56:40 +0100

    implement module for generating content of menu page

---
+++ src/menu.js
@@ -0,0 +1,50 @@
+export default function generateContent() {
+    const menuContainer = document.createElement("div");
+    menuContainer.classList.add("menu-container");
+
+    const title = document.createElement("h1");
+    title.classList.add("menu-title");
+    title.innerText = "The Menu";
+
+    menuContainer.appendChild(title);
+    menuContainer.appendChild(generateItemsList());
+}
+
+function generateItemsList() {
+    const list = document.createElement("div");
+    list.classList.add("menu-items");
+
+    const yolognese = generateItem(
+        "Spaghetti YOLOgnese",
+        "Classic spaghetti with a rich, meaty bolognese sauce that’ll have you saying “YOLO” with every bite."
+    );
+
+    const penne = generateItem(
+        "Penne for Your Thoughts",
+        "Creamy Alfredo sauce over perfectly cooked penne, topped with a sprinkle of existential bliss."
+    );
+
+    const risotto = generateItem(
+        "Risotto: The OG Comfort Food",
+        "Creamy, dreamy, and perfectly seasoned. You won’t want to share, and we don’t blame you."
+    );
+
+    list.appendChild(yolognese);
+    list.appendChild(penne);
+    list.appendChild(risotto);
+
+    return list;
+}
+
+function generateItem(title, description) {
+    const item = document.createElement("div");
+    item.classList.add("menu-item");
+
+    const title = document.createElement("h2");
+    title.classList.add("menu-item-title");
+
+    const description = document.createElement("p");
+    description.classList.add("menu-item-description");
+
+    return item;
+}
