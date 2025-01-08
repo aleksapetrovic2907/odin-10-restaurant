61ea2c392267eaa862a5a8e93c53a1c8886b6c22
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-08 20:13:34 +0100

    fix contact data not being appended to data parent

--- src/contactUs.js
+++ src/contactUs.js
@@ -26,18 +26,24 @@ function generateContactData() {
     locationTitle.innerText = "We’re conveniently located at:"
     const locationValue = document.createElement("p");
     locationValue.innerText = "123 Pasta Lane, Saucyville, Planet Italia (Turn left at the giant meatball statue, you can’t miss it!)";
+    location.appendChild(locationTitle);
+    location.appendChild(locationValue);
 
     const phone = document.createElement("div");    
     const phoneTitle = document.createElement("h2");
     phoneTitle.innerText = "Got a question? Need reservations? Want to hear a random pizza fact? Call us at:"
     const phoneValue = document.createElement("p");
     phoneValue.innerText = "1-800-ILL-TALY (Available 24/7 unless we're on our pizza break).";
+    phone.appendChild(phoneTitle);
+    phone.appendChild(phoneValue);
 
     const hours = document.createElement("div");    
     const hoursTitle = document.createElement("h2");
     hoursTitle.innerText = "We’re open when you’re hungry and close when we’ve run out of sauce. But officially:";
     const hoursValue = document.createElement("p");
     hoursValue.innerText = "Monday to Saturday: 11 AM – 11 PM";
+    hours.appendChild(hoursTitle);
+    hours.appendChild(hoursValue);
 
     contactDataContainer.appendChild(location);
     contactDataContainer.appendChild(phone);
